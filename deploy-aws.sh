4bf7d4a77dc1f62ab5169394d3cb0253d464e10b
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2019-03-29 01:18:44 +0100

    aws compatible base-href +  version 0.0.1

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 VERSION=$1
-if [ $(aws s3 ls orderin-ui-host/$VERSION/ | wc -l) -gt 0 ]; then
+if [ $(aws s3 ls orderin-ui-host/$VERSION/host/ | wc -l) -gt 0 ]; then
   echo "$VERSION already deployed";
 else
-  ng build --prod --configuration=production
-  aws s3 cp dist/orderin-host s3://orderin-ui-host/$VERSION --recursive
+  ng build --prod --configuration production
+  aws s3 cp dist/orderin-host s3://orderin-ui-host/$VERSION/host/ --recursive
 fi
