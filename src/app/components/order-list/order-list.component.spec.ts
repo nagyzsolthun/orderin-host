695aadecdfdc3e69ee02b7250ba1a15a1c9a9109
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2019-08-11 11:16:48 +0200

    order-list.component.spec.ts + order.service.spec.ts

--- src/app/components/order-list/order-list.component.spec.ts
+++ src/app/components/order-list/order-list.component.spec.ts
@@ -1,25 +1,44 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderListComponent } from './order-list.component';
+import Order from 'src/app/domain/Order';
+import { of } from 'rxjs';
+import { OrderService } from 'src/app/services/order.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+class MockOrderService {
+  getOrders() {
+    const order1 = Order.fromJson({id:"id1", counter:1, orderItems:[], tableName:{en:"table1"}});
+    const order2 = Order.fromJson({id:"id2", counter:2, orderItems:[], tableName:{en:"table2"}});
+    return of([order1, order2]);
+  }
+}
 
 describe('OrderListComponent', () => {
-  let component: OrderListComponent;
   let fixture: ComponentFixture<OrderListComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OrderListComponent ]
+      declarations: [ OrderListComponent ],
+      providers: [
+        { provide: OrderService, useClass: MockOrderService },
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OrderListComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render orders', () => {
+    const orderElements = compiled.querySelectorAll(".order");
+    expect(orderElements.length).toBe(2);
+    expect(orderElements[0].innerHTML).toBe("001");
+    expect(orderElements[1].innerHTML).toBe("002");
   });
 });
--- src/app/services/order.service.spec.ts
+++ src/app/services/order.service.spec.ts
@@ -1,12 +1,93 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 
 import { OrderService } from './order.service';
+import { of, ReplaySubject } from 'rxjs';
+import Order from '../domain/Order';
+import { DataService } from './data.service';
+import { RouteParamsService } from './route-params.service';
+import { HttpClient } from '@angular/common/http';
+import { NgZone } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+class MockRouteParamsService {
+  venueId() { return of("venueId") }
+}
+
+class MockDataService {
+  user$ = new ReplaySubject<string>(1);
+  user() { return this.user$; }
+}
+
+class MockHttpClient {
+  get() {
+    const order1 = Order.fromJson({id:"id1", counter:1, orderItems:[], tableName:{en:"table1"}});
+    const order2 = Order.fromJson({id:"id2", counter:2, orderItems:[], tableName:{en:"table2"}});
+    return of([order1, order2]);
+  }
+}
+
+class MockEventSource {
+  static lastInstance: MockEventSource;
+  constructor(url: string) {
+    MockEventSource.lastInstance = this;
+  }
+
+  onmessage: (message: any) => any;
+}
+
 
 describe('OrderService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: OrderService = TestBed.get(OrderService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        OrderService,
+        { provide: RouteParamsService, useClass: MockRouteParamsService },
+        { provide: DataService, useClass: MockDataService },
+        { provide: HttpClient, useClass: MockHttpClient },
+      ]
+    });
   });
+
+  beforeEach(() => {
+    window["EventSource"] = MockEventSource;
+    MockEventSource.lastInstance = null;
+  })
+
+  it('waits for authentication before fetching orders', async(() => {
+    const dataService = TestBed.get(DataService) as MockDataService;
+    const httpClient = TestBed.get(HttpClient) as MockHttpClient;
+    spyOn(httpClient, "get").and.callThrough();
+
+    const service = TestBed.get(OrderService) as OrderService;
+    expect(httpClient.get).toHaveBeenCalledTimes(0);
+
+    dataService.user$.next(null);
+    expect(httpClient.get).toHaveBeenCalledTimes(0);
+
+    dataService.user$.next("user");
+    expect(httpClient.get).toHaveBeenCalledTimes(1);
+  }));
+
+  it('adds orders coming from SSE', async(() => {
+    const dataService = TestBed.get(DataService) as MockDataService;
+    const service = TestBed.get(OrderService) as OrderService;
+
+    dataService.user$.next("user");
+    service.getOrders().pipe(first()).subscribe(orders => {
+      expect(orders.length).toBe(2);
+      expect(orders[0].id).toBe("id1");
+      expect(orders[1].id).toBe("id2");
+    });
+
+    const order3 = Order.fromJson({id:"id3", counter:3, orderItems:[], tableName:{en:"table3"}});
+    const orderEventsSource = MockEventSource.lastInstance;
+    orderEventsSource.onmessage({ data: JSON.stringify(order3) });
+    service.getOrders().pipe(first()).subscribe(orders => {
+      expect(orders.length).toBe(3);
+      expect(orders[0].id).toBe("id1");
+      expect(orders[1].id).toBe("id2");
+      expect(orders[2].id).toBe("id3");
+    });
+
+  }));
 });
