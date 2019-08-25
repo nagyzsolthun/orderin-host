78081d21c3d057fea6d1fc0bc41466486ae94c26
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2019-08-25 14:15:20 +0200

    order-list-item navigate on click

--- src/app/components/order-list-item/order-list-item.component.spec.ts
+++ src/app/components/order-list-item/order-list-item.component.spec.ts
@@ -2,7 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderListItemComponent } from './order-list-item.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 import Order from 'src/app/domain/Order';
+import { of } from 'rxjs';
+import { RouteParamsService } from 'src/app/services/route-params.service';
+
+class MockRouteParamsService {
+  venueId() { return of("venueId"); }
+}
 
 describe('OrderListItemComponent', () => {
   let fixture: ComponentFixture<OrderListItemComponent>;
@@ -11,7 +18,11 @@ describe('OrderListItemComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [ OrderListItemComponent ],
+      providers: [
+        { provide: RouteParamsService, useClass: MockRouteParamsService },
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
@@ -20,13 +31,18 @@ describe('OrderListItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(OrderListItemComponent);
     component = fixture.componentInstance;
+    component.order = Order.fromJson({id:"id", counter:"001", orderItems:[], tableName:{en:"table"}});
+    fixture.detectChanges();
     compiled = fixture.debugElement.nativeElement;
   });
 
   it('should show counter', () => {
-    component.order = Order.fromJson({id:"id", counter:"001", orderItems:[], tableName:{en:"table"}});
-    fixture.detectChanges();
     expect(compiled.querySelector("div").innerText).toBe("001");
+  });
+
+  it('should redirect when clicked', () => {
+    console.log(compiled.querySelector("a"))
+    expect(compiled.querySelector("a").getAttribute("href")).toBe("/venueId/order/001");
   });
 
 });
--- src/app/components/order-list-item/order-list-item.component.ts
+++ src/app/components/order-list-item/order-list-item.component.ts
@@ -1,15 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import Order from 'src/app/domain/Order';
+import { Observable } from 'rxjs';
+import { RouteParamsService } from 'src/app/services/route-params.service';
 
 @Component({
   selector: 'app-order-list-item',
   templateUrl: './order-list-item.component.html',
   styleUrls: ['./order-list-item.component.css']
 })
-export class OrderListItemComponent {
+export class OrderListItemComponent implements OnInit{
 
   @Input() order: Order;
+  venueId$: Observable<string>;
 
-  constructor() { }
+  constructor(private routeParamsService: RouteParamsService) { }
+
+  ngOnInit() {
+    this.venueId$ = this.routeParamsService.venueId();
+  }
 
 }
